5c649aa23c474df00e41e664b9bf7c07679482ba
soukainanachib <soukaina.nachib23@gmail.com>
2024-05-24 22:25:26 +0100

    Create BaseModel from dictionary

--- console.py
+++ console.py
@@ -17,12 +17,45 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """ Quit command to exit the program"""
-        return True
+        exit()
     
     def do_EOF(self, arg):
         """ EOF signal to exit the program"""
         print("")
-        return True
+        exit()
+    
+    def do_create(self, args):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        if not args:
+            print("Class name Missing")
+            return
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exit  **")
+            return
+        new_instatce = eval[args]()
+        new_instatce.save()
+        print(new_instatce.id)
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
